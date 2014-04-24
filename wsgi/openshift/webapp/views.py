28eeae40dc602e301c9388dd647d07a57bee65bc
Lorenzo <lorenzo@linux-74kc.site>
2014-04-24 13:24:04 +0200

    Errors corrected

--- wsgi/openshift/webapp/views.py
+++ wsgi/openshift/webapp/views.py
@@ -5,5 +5,5 @@ def start(request):
     
     params = {}
     params['keywords'] = 'explore space planets star journey satellites exploration solar system simulation play'
-    params['status'] = js
+
     return render_to_response('home/destinations.html', params)
