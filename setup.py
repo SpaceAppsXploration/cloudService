8aa4349284219dfecd3fe955959dd4340e3fb276
Lorenzo <lorenzo@linux-74kc.site>
2014-05-04 18:02:32 +0200

    Django version 1.6.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='Project Chronos',
     version='1.0',
-    description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
-    url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django<=1.4'],
+    description='Xploration Apps and REST',
+    author='Space Apps Rome 2014 Project Chronos',
+    author_email='@XplorationApp',
+    url='https://2014.spaceappschallenge.org/project/chronos/',
+    install_requires=['Django==1.6.2'],
 )
