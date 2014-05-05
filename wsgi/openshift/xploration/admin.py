41241cb7d439928adfa21bd499f82da329673054
Lorenzo <tunedconsulting@gmail.com>
2014-05-05 14:57:22 +0200

    Filtered MissionMenu In AddDetails Admin Panel

--- wsgi/openshift/xploration/admin.py
+++ wsgi/openshift/xploration/admin.py
@@ -19,15 +19,36 @@ class DetailsAdmin(admin.ModelAdmin):
         if db_field.name == 'body':
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "mission":
+            
+            d = {}
+            for a in Missions.objects.all():
+                if not d.get(a.codename):
+                    d[a.codename] = a.id  
+            #Alldistinct.add((o.codename, o.id))
+            
+            Alldistinct = set(d.values())
+            Alldistinct = Missions.objects.filter(id__in=Alldistinct)
+            kwargs["queryset"] = Alldistinct
+        return super(DetailsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 class PlanetsAdmin(admin.ModelAdmin):
     pass    
 
 class PayloadBusTypesAdmin(admin.ModelAdmin):
-    pass
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(PayloadBusTypesAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'description':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
 
 class PayloadBusCompsAdmin(admin.ModelAdmin):
-    pass
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(PayloadBusCompsAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'description':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
  
 admin.site.register(Targets, TargetsAdmin)
 admin.site.register(Missions, MissionsAdmin)
