ca5cbd34ae378773d1118b40aaa6f800bf4ff53d
Lorenzo <tunedconsulting@gmail.com>
2014-05-06 22:08:10 +0200

    Missions Images

--- wsgi/openshift/webapp/appdata/sim_missions.py
+++ wsgi/openshift/webapp/appdata/sim_missions.py
@@ -2,31 +2,39 @@ missions = [
 {
 'slug': 'earth_obs',
 'name': 'Earth Observation',
-'description': 'Look down at Earth\'s surface. It\'s a World Of Wonders!'
+'description': 'Look down at Earth\'s surface. It\'s a World Of Wonders!',
+'image_url': '/static/images/webapp/Earth_obs.png'
+
 },
 {
 'slug': 'cel_body_obs',
 'name': 'Solar System\'s Bodies Observation',
-'description': 'We have nice neighbours, let\'s keep in touch witht them!'
+'description': 'We have nice neighbours, let\'s keep in touch witht them!',
+'image_url': '/static/images/webapp/Near.png'
+
 },
 {
 'slug': 'deep_space_obs',
 'name': 'Deep Space Observation',
-'description': 'We can learn a lot contemplating other systems'
+'description': 'We can learn a lot contemplating other systems',
+'image_url': '/static/images/webapp/Deep.png'
 },
 {
 'slug': 'atm_analysis',
 'name': 'Atmospheric Analysis',
-'description': 'Love is in the air?'
+'description': 'Love is in the air?',
+'image_url': '/static/images/webapp/Analysis.png'
 },
 {
 'slug': 'sample_collect',
 'name': 'Collect Surface Samples',
-'description': 'We will rock it!'
+'description': 'We will rock it!',
+'image_url': '/static/images/webapp/Sample.png'
 },
 {
 'slug': 'telecom',
 'name': 'Earth or Orbital Telecommunication',
-'description': 'What if I would like to talk to Antartics?'
+'description': 'What if I would like to talk to Antartics?',
+'image_url': '/static/images/webapp/Communication.png'
 }
 ]
