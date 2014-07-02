d4a158c8424a4f2fc9071b643d245e494b7c4d36
Lorenzo <tunedconsulting@gmail.com>
2014-07-02 23:56:09 +0200

    Corrected Admin.Py To Handle Fields

--- wsgi/openshift/chronos/admin.py
+++ wsgi/openshift/chronos/admin.py
@@ -6,15 +6,18 @@ from models import Targets, Missions, Details, Planets, PayloadBusTypes, Payload
 from django import forms
 from django.forms import SelectMultiple
 
+
 class TargetsAdmin(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
         formfield = super(TargetsAdmin, self).formfield_for_dbfield(db_field, **kwargs)
         if db_field.name == 'characteristics' or db_field.name == 'curiosities' or db_field.name == 'sim_related':
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
-        
+
+
 class MissionsAdmin(admin.ModelAdmin):
     pass
+
 
 class DetailsAdmin(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
@@ -22,10 +25,11 @@ class DetailsAdmin(admin.ModelAdmin):
         if db_field.name == 'body':
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        '''
+        """
         Show only unique values from Missions, eg.(mission,target) coupling
-        '''
+        """
         if db_field.name == "mission":
             
             d = {}
@@ -33,13 +37,15 @@ class DetailsAdmin(admin.ModelAdmin):
                 if not d.get(a.codename):
                     d[a.codename] = a.id  
             
-            Alldistinct = set(d.values())
-            Alldistinct = Missions.objects.filter(id__in=Alldistinct)
-            kwargs["queryset"] = Alldistinct
+            allDistinct = set(d.values())
+            allDistinct = Missions.objects.filter(id__in=allDistinct)
+            kwargs["queryset"] = allDistinct
         return super(DetailsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 class PlanetsAdmin(admin.ModelAdmin):
     pass    
+
 
 class PayloadBusTypesAdmin(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
@@ -55,8 +61,10 @@ class PayloadBusCompsAdmin(admin.ModelAdmin):
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
 
+
 class SciDataAdmin(admin.ModelAdmin):
     formfield_overrides = { models.ManyToManyField: {'widget': SelectMultiple(attrs={'size':'12'})}, }
+
     def formfield_for_dbfield(self, db_field, **kwargs):
         formfield = super(SciDataAdmin, self).formfield_for_dbfield(db_field, **kwargs)
         if db_field.name == 'body':
@@ -64,6 +72,14 @@ class SciDataAdmin(admin.ModelAdmin):
         if db_field.name == 'comment':
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        Show only values of type field from SciData
+        """
+        if db_field.name == "related_to":
+            kwargs["queryset"] = SciData.objects.all().filter(data_type=4)
+        return super(SciDataAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Targets, TargetsAdmin)
 admin.site.register(Missions, MissionsAdmin)
